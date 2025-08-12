c9c825c9800d88b4cdcd36caea1e77c0955e2b05
ThakshinnSuriya <thakshinnsuriya@gmail.com>
2025-08-12 21:44:01 +0530

    Day 2 of django model forms and admin pannel

--- firstproject/firstapp/models.py
+++ firstproject/firstapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    available = models.BooleanField(default=True)
+
+   
+class Reservation(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    reservation_time = models.DateTimeField(auto_now=True)
+    guest_count = models.PositiveIntegerField()
+    comments = models.TextField(max_length=1000)
+
+    
