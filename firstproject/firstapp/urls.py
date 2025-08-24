bd6032a05eacaba45f052fc27d2c03cdab8f885a
Thakshinn Suriya <thakshinnsuriya@gamil.com>
2025-08-24 05:58:12 +0530

    ADDED TEMPLATE AND ALTERED VIEWS

--- firstproject/firstapp/urls.py
+++ firstproject/firstapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('class',views.hello.as_view(), name='hello_class'),
     path('function', views.hello_world, name='hello_function'),
+    path('reservation', views.home),
 ]
--- firstproject/firstapp/views.py
+++ firstproject/firstapp/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from .forms import ReservationForm
 def hello_world(request):
-    return httpResponse("Hello, World!")
+    return HttpResponse("Hello, World!")
 
 class hello(View):
     def get(self, request):
         return HttpResponse("Hello, World from Class-Based View!")
+
+def home(request):
+    form=ReservationForm()
+    if request.method =='POST':
+        form =ReservationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("Success")
+    return render(request, 'index.html',{'form' :form})        
     
     
 
